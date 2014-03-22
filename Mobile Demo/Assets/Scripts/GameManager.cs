ef0b4fa72f8c869538d5929cb4356933463358e5
estorm <elgarstorm@gmail.com>
2014-03-22 17:07:57 +1300

    Added Player prefab and GameManager

---
+++ Mobile Demo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private Player[] players;
+
+	// Use this for initialization
+	void Start () {
+		players = FindObjectsOfType(typeof(Player)) as Player[];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Mobile Demo/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public bool isHuman = false;
+	public string displayName = "Player";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
